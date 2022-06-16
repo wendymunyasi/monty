bca19dc898f4b12f3ee7ad96289bdb8879d9d291
wendy munyasi <wendymunyasi@gmail.com>
2022-06-16 21:23:29 +0300

    Implement the pop opcode

--- get_po.c
+++ get_po.c
@@ -15,6 +15,7 @@ void get_po(stack_t **stack, char *op, unsigned int line_number)
 		{"push", _push},
 		{"pall", _pall},
 		{"pint", _pint},
+		{"pop", _pop},
 		{NULL, NULL}
 	};
 
--- monty.h
+++ monty.h
@@ -16,7 +16,6 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-#define UNUSED(x) (void)(x)
 #define TRUE 1
 #define FALSE 0
 /**
@@ -71,5 +70,6 @@ void _push(stack_t **stack, unsigned int line_number);
 void _pall(stack_t **stack, unsigned int line_number);
 int interpreter(char *num_string, unsigned int line_number);
 void _pint(stack_t **stack, unsigned int line_number);
+void _pop(stack_t **stack, unsigned int line_number);
 
 #endif /* MONTY */
